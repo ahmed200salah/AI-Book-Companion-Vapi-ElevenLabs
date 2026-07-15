b57ef2db6b81d67c588a3305cf78027ba2caab0b
Ahmed mohamed salah <erewrrewrewr15@gmail.com>
2026-07-15 18:57:58 +0300

    NavBar finished next step auth

---
+++ src/components/navbar.tsx
@@ -0,0 +1,54 @@
+"use client";
+
+import Link from "next/link";
+import Image from "next/image";
+import { usePathname } from "next/navigation";
+import { cn } from "@/lib/utils";
+
+const navItems = [
+  { label: "Library", href: "/" },
+  { label: "Add New", href: "/books/new" },
+  { label: "Pricing", href: "/subscriptions" },
+];
+
+const Navbar = () => {
+  const pathName = usePathname();
+
+  return (
+    <header className="w-full fixed z-50 bg-('--bg-primary')">
+      <div className="flex justify-between items-center py-4 wrapper navbar-hight">
+        <Link href="/" className="text-2xl font-bold flex gap-0.5 items-center">
+          <Image
+            src="/assets/logo.png"
+            alt="bookfied"
+            width={42}
+            height={26}
+            className="rounded-full"
+          />
+          <span className="logo-text">Bookified</span>
+        </Link>
+        <nav className="w-fit flex gap-7.5 items-center">
+          {navItems.map(({ label, href }) => {
+            const isActive =
+              pathName === href || (href !== "/" && pathName.startsWith(href));
+
+            return (
+              <Link
+                href={href}
+                key={label}
+                className={cn(
+                  "nav-link-base",
+                  isActive ? "nav-link-active" : "text-black hover:opacity-70",
+                )}
+              >
+                {label}
+              </Link>
+            );
+          })}
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Navbar;
